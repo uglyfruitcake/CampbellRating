66798990dc473332831ec72da31a74b0ee472087
James Campbell <james.campbell@tanti.org.uk>
2014-04-13 15:48:42 +0100

    simplifies crating

--- CampbellRating.py
+++ CampbellRating.py
@@ -1,6 +1,14 @@
-import sys
 import math
-import time
+from operator import itemgetter
+
+
+def campbell_rating(limit, power):
+    ratings = []
+    for i in range(1, limit):
+        r = (num_factors(i) ** power)/float(i)
+        ratings.append([i, r])
+
+    return sorted(ratings, key=itemgetter(2))[1]
 
 
 def is_factor(factor, product):
@@ -10,55 +18,20 @@ def is_factor(factor, product):
         return False
 
 
-def campbell_rating(number):
+def num_factors(number):
     possible_factor = 2
     number_factors = 2
+    sqrt = math.sqrt(number)
 
-    while (possible_factor < math.sqrt(number)):
+    while possible_factor < sqrt:
         if is_factor(possible_factor, number):
             number_factors += 2
-
         possible_factor += 1
 
-    if is_factor(math.sqrt(number), number):
+    if is_factor(sqrt, number):
         number_factors += 1
 
     if number == 1:
         return 1
     else:
-        return (number_factors ** 2) / float(number)
-
-
-def format_time(seconds):
-    elapsed_hours = int(seconds // 3600)
-    elapsed_minutes = int((seconds % 3600) // 60)
-    elapsed_seconds = (seconds % 3600) % 60
-    return "{0} Hours {1} Minutes {2} Seconds".format(elapsed_hours, elapsed_minutes, elapsed_seconds)
-
-
-def main():
-    target = int(sys.argv[1])
-    threshold = float(sys.argv[2])
-    number = 1
-    hits = 0
-    start_time = time.time()
-
-    while (number <= target):
-        rating = campbell_rating(number)
-
-        if rating > threshold:
-            hits += 1
-            print "{0} has a Campbell rating of {1}".format(number, rating)
-
-        number += 1
-
-    print format_time(time.time() - start_time)
-    print "Found {0} results between 1 and {1} with Campbell rating above {2}".format(hits, target, threshold)
-
-
-if len(sys.argv) < 3:
-    print "Usage: CampbellRatings.py <target> <threshold>"
-    print "target - The maximum number for which the Campbell rating will be calculated"
-    print "threshold - The minimum Campbell rating which will be reported"
-else:
-    main()
+        return number
